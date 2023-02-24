044d7ae75c26617ed57d588660f32dae3f7e5bcf
MOVZX <movzx@yahoo.com>
2023-02-24 13:39:02 +0700

    Fix missed 1 teeth model
    Signed-off-by: MOVZX <movzx@yahoo.com>

--- client/cl_main.lua
+++ client/cl_main.lua
@@ -554,7 +554,7 @@ function OpenMouthMenu()
     TaskPlayAnim(PlayerPedId(), "FACE_HUMAN@GEN_MALE@BASE", "Face_Dentistry_Loop", 1090519040, -4, -1, 17, 0, 0, 0, 0, 0, 0)
 
     local elements = {
-        {label = RSG.Texts.Teeth,          value = CreatorCache["teeth"] or 0,      category = "teeth",      desc = "", type = "slider", min = 0, max = 6},
+        {label = RSG.Texts.Teeth,          value = CreatorCache["teeth"] or 1,      category = "teeth",      desc = "", type = "slider", min = 1, max = 7},
         {label = RSG.Texts.Width,          value = CreatorCache["mouth_width"] or 0,      category = "mouth_width",      desc = "", type = "slider", min = -100, max = 100, hop = 5},
         {label = RSG.Texts.Depth,          value = CreatorCache["mouth_depth"] or 0,      category = "mouth_depth",      desc = "", type = "slider", min = -100, max = 100, hop = 5},
         {label = RSG.Texts.UP_DOWN,        value = CreatorCache["mouth_x_pos"] or 0,      category = "mouth_x_pos",      desc = "", type = "slider", min = -100, max = 100, hop = 5},
