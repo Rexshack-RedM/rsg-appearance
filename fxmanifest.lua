db4519301db360fe79f7e1462830d5c687235804
RexshackGaming <rexshackuk@gmail.com>
2023-12-12 07:28:58 +0000

    version bump

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -3,7 +3,7 @@ rdr3_warning 'I acknowledge that this is a prerelease build of RedM, and I am aw
 game 'rdr3'
 
 description 'rsg-appearance'
-version '1.0.1'
+version '1.0.2'
 
 client_scripts {
     'utils/hairs.lua',
