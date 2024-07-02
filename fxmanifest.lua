1e024487405bc7a79996576aaa06a20d99d75c58
RexshackGaming <rexshackuk@gmail.com>
2024-07-02 17:04:37 +0100

    fix freeze thanks to 🎀Luke🎀

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -2,10 +2,9 @@ fx_version 'cerulean'
 rdr3_warning 'I acknowledge that this is a prerelease build of RedM, and I am aware my resources *will* become incompatible once RedM ships.'
 game 'rdr3'
 lua54 'yes'
-use_experimental_fxv2_oal 'yes'
 
 description 'rsg-appearance'
-version '2.3.2'
+version '2.3.3'
 
 shared_scripts {
     '@ox_lib/init.lua',
